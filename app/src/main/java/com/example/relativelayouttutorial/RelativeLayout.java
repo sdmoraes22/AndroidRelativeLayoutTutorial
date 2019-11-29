571d26ba9969164ad863342e55e4d1f75c42e2e0
sdmoraes22 <cmoraes.ti@gmail.com>
2019-11-29 17:09:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/relativelayouttutorial/RelativeLayout.java
@@ -0,0 +1,15 @@
+package com.example.relativelayouttutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RelativeLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
